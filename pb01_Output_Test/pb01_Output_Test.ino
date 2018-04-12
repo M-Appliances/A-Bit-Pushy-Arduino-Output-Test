b4ef225e7f3ad7a1f3ddff6ef74d3959771df5a4
Thomas Cooper <tc@coope.biz>
2018-04-12 23:02:47 +0100

    Mac Address Added & output slowed to make more legible

--- pb01_Output_Test/pb01_Output_Test.ino
+++ pb01_Output_Test/pb01_Output_Test.ino
@@ -4,7 +4,10 @@
 
   --- A Bit Pushy ---
 
-  RELEASE V 1.0.1
+  RELEASE V 1.0.2
+
+  - V 1.0.1 - Minor comment alterations
+  - V 1.0.2 - Mac Address Added & output slowed to make more legible
 
   Distributed under GNU General Public License V3
 
@@ -18,9 +21,10 @@
 
 int buzzer = 14;          // This is the pin output leading to your Buzzer
 int LED = 13;             // This is the pin output leading to your blue status LED
-int successLED = 15;      // This is the pin output leading to your green success LED
-int alertLED = 12;        // This is the pin output leading to the red alert LED
+int successLED = 12;      // This is the pin output leading to your green success LED
+int alertLED = 15;        // This is the pin output leading to the red alert LED
 
+byte mac[6];
 
 void setup() {
   Serial.begin(115200);
@@ -30,13 +34,17 @@ void setup() {
   pinMode(successLED, OUTPUT);
   pinMode(alertLED, OUTPUT);
 
+  WiFi.macAddress(mac);
+  
   Serial.println("");
+  macOutput();
   outputTest();
   outputTest();
   outputTest();
   outputTest();
   outputTest();
-  Serial.println("Night!");
+  
+  Serial.println("Going to sleep now.");
   ESP.deepSleep(0);
 
 }
@@ -47,28 +55,47 @@ void loop() {
 
 }
 
-void outputTest() {
+void outputTest() { 
   Serial.println("LED Blue");
   digitalWrite(LED, HIGH);
-  digitalWrite(buzzer, HIGH);
-  delay(250);
-  digitalWrite(LED, LOW);
   digitalWrite(buzzer, LOW);
-  delay(250);
+  delay(2000);
+  digitalWrite(LED, LOW);
+  digitalWrite(buzzer, HIGH);
+  delay(500);
 
   Serial.println("LED Green");
   digitalWrite(successLED, HIGH);
-  digitalWrite(buzzer, HIGH);
-  delay(250);
-  digitalWrite(successLED, LOW);
   digitalWrite(buzzer, LOW);
-  delay(250);
+  delay(2000);
+  digitalWrite(successLED, LOW);
+  digitalWrite(buzzer, HIGH);
+  delay(500);
 
   Serial.println("LED Red");
   digitalWrite(alertLED, HIGH);
-  digitalWrite(buzzer, HIGH);
-  delay(250);
-  digitalWrite(alertLED, LOW);
   digitalWrite(buzzer, LOW);
-  delay(250);
+  delay(2000);
+  digitalWrite(alertLED, LOW);
+  digitalWrite(buzzer, HIGH);
+  delay(1500);
+
+  Serial.println("--------");
+  digitalWrite(buzzer, LOW);
 }
+
+void macOutput() {
+  Serial.print("MAC: ");
+  Serial.print(mac[5],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[1],HEX);
+  Serial.print(":");
+  Serial.println(mac[0],HEX);
+}
+
